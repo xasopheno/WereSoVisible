acf2b15540558b0a762f0bf1cfc361f416ee71b0
Danny Meyer <Danny.Meyer@gmail.com>
2019-04-04 20:55:02 -0400

    animating on

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,15 @@ import * as THREE from 'three';
 import song from './tokyo';
 const OrbitControls = require('three-orbit-controls')(THREE);
 
-export default class App extends React.Component {
+interface Props {}
+
+interface State {
+  n: any;
+  height: any;
+  width: any;
+}
+
+export default class App extends React.Component<Props, State> {
   private three: any;
   private scene: any;
   private camera: any;
@@ -20,12 +28,14 @@ export default class App extends React.Component {
   private controls: any;
   private data: any;
   private n: number;
+  private interval: any;
   constructor(props: any) {
     super(props);
     this.theta = 0.0;
     this.n = 0;
 
     this.state = {
+      n: 0,
       height: window.innerHeight,
       width: window.innerWidth,
     };
@@ -48,36 +58,61 @@ export default class App extends React.Component {
   //   this.camera.updateProjectionMatrix();
   //   this.renderer.setSize(window.innerWidth, window.innerHeight);
   // }
-  public readJson() {
-    return song.data;
+
+  public readJson = async () => {
+    return await song.data;
   }
   public render_points() {
-    for (let i = this.n; i < this.data.length; i++) {
-      if (this.data[i].z > 0.0 && this.data[i].y > 20.0 && this.data[i].event_type === 'On') {
-        const point = this.data[i];
-        const object = new THREE.Mesh(
-          this.geometry,
-          new THREE.MeshLambertMaterial({ color: (point.voice / 50) * 0xffffff })
-        );
-        object.position.x = point.x * 500;
-        object.position.y = Math.log(point.y) * 200 - 1000;
-        object.position.z = point.t * 150;
-        // object.rotation.x = Math.random() * 2 * Math.PI;
-        // object.rotation.y = Math.random() * 2 * Math.PI;
-        // object.rotation.z = Math.random() * 2 * Math.PI;
-        const scale = Math.exp(point.z) - 0.5;
-        object.scale.x = scale;
-        object.scale.y = scale;
-        object.scale.z = point.l * scale;
+    // for (let i = this.state.n; i < this.state.n + 100; i++) {
+    // for (let i = 0; i < this.data.length; i++) {
+    //   const point = this.data[i];
+    const dl = this.data.length;
+    for (let i = 0; i < 300; i++) {
+      if (this.data.length > 0) {
+        let point = this.data.shift();
+        // console.log(point.t)
+        if (point.z > 0.0 && point.y > 20.0 && point.event_type === 'On') {
+          const object = new THREE.Mesh(
+            this.geometry,
+            new THREE.MeshLambertMaterial({ color: (point.voice / 50) * 0xffffff })
+          );
 
-        this.scene.add(object);
-        this.n += 100;
+          // const rand = point.voice * 0.2 * (Math.random() * 2) - 1; // * rand
+          // const rand = point.voice * 0.01 * (Math.random() * 2) - 1;
+          // const rand = 4 * (Math.random() * 2 - 1);
+          const rand = 1.0
+
+          object.position.x = point.x * 500 + rand;
+          object.position.y = Math.log(point.y) * 200 - 1000;
+          object.position.z = point.t * 150 - 3500;
+          // object.rotation.x = Math.random() * 2 * Math.PI;
+          // object.rotation.y = Math.random() * 2 * Math.PI;
+          // object.rotation.z = Math.random() * 2 * Math.PI;
+          const scale = Math.exp(point.z) - 0.5;
+          object.scale.x = scale;
+          object.scale.y = scale;
+          object.scale.z = point.l * scale;
+
+          this.scene.add(object);
+        }
       }
     }
   }
 
-  public componentDidMount() {
-    this.data = this.readJson();
+  public update_n() {
+      // this.setState({
+      //   ...this.state,
+      //   n: this.state.n + 1000,
+      // });
+    // if (this.state.n < this.data.length - 2000) {
+    //     console.log(this.state.n, this.data.length - 2000)
+    //     this.render_points();
+    //   }
+  }
+
+  public componentDidMount = async () => {
+    this.interval = setInterval(() => this.update_n(), 100);
+    this.data = await this.readJson();
     // this.updateDimensions();
     // window.addEventListener('resize', this.updateDimensions.bind(this));
     this.container = document.createElement('div');
@@ -92,12 +127,13 @@ export default class App extends React.Component {
     this.camera = new THREE.PerspectiveCamera(70, window.innerWidth / window.innerHeight, 1, 10000);
     this.controls = new OrbitControls(this.camera);
 
-    this.camera.position.set(0, 0, 600);
+    this.camera.position.set(-4000, 1000, 0);
     this.controls.update();
 
     // this.camera.position.z = 600;
 
     this.scene = new THREE.Scene();
+    this.camera.lookAt(this.scene.position);
     this.scene.background = new THREE.Color(0x404040);
     const light = new THREE.AmbientLight(0xffffff); // soft white light
     this.scene.add(light);
@@ -106,6 +142,9 @@ export default class App extends React.Component {
     this.scene.add(light);
     this.geometry = new THREE.BoxBufferGeometry(20, 20, 20);
 
+    console.log(this.data.length);
+    this.render_points();
+    console.log(this.data.length);
 
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setPixelRatio(window.devicePixelRatio);
@@ -134,7 +173,6 @@ export default class App extends React.Component {
     // this.controls.update();
 
     this.render_points();
-
     this.render();
     this.controls.update();
 
