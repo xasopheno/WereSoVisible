a90374bc514b212c091dbb61d94f84ff25f8e2ab
Danny Meyer <Danny.Meyer@gmail.com>
2020-01-19 00:28:53 -0500

    compose syntax highlighting working wow

--- src/Compose.tsx
+++ src/Compose.tsx
@@ -1,14 +1,60 @@
-import * as React from 'react';
+import React, { useState, ChangeEvent } from 'react';
+import styled from 'styled-components';
+import AceEditor from 'react-ace';
+
+import 'ace-builds/src-noconflict/mode-elixir';
+import 'ace-builds/src-noconflict/theme-terminal';
+import 'ace-builds/src-noconflict/keybinding-vim';
+
+const Title = styled.h1`
+  text-align: center;
+  padding-top: 10px;
+  color: #edd;
+`;
+const TextArea = styled.textarea`
+  height: 70%;
+  width: 80%;
+  font-size: 20px;
+  margin-left: 10%;
+`;
+const Space = styled.div`
+  background-color: #454343;
+  height: 100vh;
+`;
 
 function Compose() {
+  const x = '{ f: 220, l: 1, g: 1, p: 0 }\n\nmain = {\n\tTm 1\n}';
+  const [language, setLanguage] = useState<string>(x);
+
   return (
-    <div
-      style={{
-        backgroundColor: '#fff',
-      }}
-    >
-      <h3>Compose</h3>
-    </div>
+    <Space>
+      <Title>WereSoCool</Title>
+      <AceEditor
+        placeholder="Placeholder Text"
+        mode="elixir"
+        theme="terminal"
+        name="wsc"
+        keyboardHandler="vim"
+        onChange={setLanguage}
+        fontSize={24}
+        showPrintMargin={true}
+        showGutter={true}
+        highlightActiveLine={true}
+        value={language}
+        setOptions={{
+          enableBasicAutocompletion: false,
+          enableLiveAutocompletion: false,
+          enableSnippets: false,
+          showLineNumbers: true,
+          tabSize: 2,
+        }}
+        style={{
+          height: '80vh',
+          width: '80vw',
+          marginLeft: '10vw',
+        }}
+      />
+    </Space>
   );
 }
 
--- src/Paths.tsx
+++ src/Paths.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { BrowserRouter as Router, Route, Switch, useParams } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Switch, Redirect } from 'react-router-dom';
 //import App from './App';
 import Compose from './Compose';
 import Play from './Play';
@@ -10,7 +10,9 @@ class Paths extends React.Component {
       <Router>
         <Switch>
           <Route path="/compose" children={<Compose />} />
+
           <Route path="/play/:id" children={<Play />} />
+          <Redirect from="/" to="/compose" />
         </Switch>
       </Router>
     );
