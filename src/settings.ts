03884e6b31ce46585cae360a873fbb0d89de42b8
Danny Meyer <Danny.Meyer@gmail.com>
2020-02-07 17:11:17 -0500

    WIP

--- src/settings.ts
+++ src/settings.ts
@@ -1,8 +1,7 @@
 const BACKEND = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:4599/api';
 
 const Settings = {
-  "backend": BACKEND;
+  backend: BACKEND,
 };
 
 export default Settings;
-
